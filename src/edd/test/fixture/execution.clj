dd93f59b819c5aa5da257bd9e18e49effa10df20
Guenter KICKINGER <guenter1.kickinger@rbinternational.com>
2021-02-26 13:21:18 +0100

    [LMSO-1735] Test executions with random queues
    Change-Id: I65f696bf0835453a51ccd1b45d711604b3fdbdfa

---
+++ src/edd/test/fixture/execution.clj
@@ -0,0 +1,44 @@
+(ns edd.test.fixture.execution
+  (:require [edd.test.fixture.dal :as f]
+            [edd.el.cmd :as cmd]
+            [edd.el.event :as event]
+            [edd.memory.event-store :as event-store]
+            [clojure.tools.logging :as log]))
+
+
+(defn process-cmd-response! [ctx cmd]
+  (log/info "apply-cmd" cmd)
+  (let [resp (f/handle-cmd (assoc ctx
+                                  :no-summary true)
+                           cmd)]
+    (log/info "apply-cmd returned" resp)
+    (doseq [id (distinct (map :id (:events resp)))]
+      (event/handle-event (assoc ctx
+                                 :apply {:aggregate-id id})))
+    resp))
+
+(defn process-next!
+  [ctx]
+  (if-let [cmd (event-store/peek-cmd!)]
+    (process-cmd-response! ctx cmd)))
+
+
+(defn process-all!
+  [ctx]
+  (loop []
+    (when (process-next! ctx)
+      (recur))))
+
+
+(defn place-cmd!
+  [& cmds]
+  (doseq [cmd cmds]
+    (if (:commands cmd)
+      (event-store/enqueue-cmd! cmd)
+      (event-store/enqueue-cmd! {:commands [cmd]}))))
+
+
+(defn run-cmd!
+  [ctx & cmd]
+  (apply place-cmd! cmd)
+  (process-all! ctx))
