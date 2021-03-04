cb13a517e9a6dce738237e1741712cad20c0da3a
Robert POFUK <robert.pofuk@rbinternational.com>
2021-03-04 12:43:29 +0100

    [LMSO-1785] Run DB migration before running tests
    Change-Id: I769ed026c54b42c6a698fc9ff2bf102d6bc0f02f

--- src/edd/common.clj
+++ src/edd/common.clj
@@ -2,9 +2,9 @@
   (:require
     [edd.el.event :as event]
     [edd.dal :as dal]
+    [edd.search :as search]
     [lambda.uuid :as uuid]
-    [lambda.request :as request]
-    [next.jdbc.result-set :as rs]))
+    [lambda.request :as request]))
 
 
 (defn get-by-id
@@ -28,6 +28,53 @@
       (swap! request/*request*
              #(assoc-in % [:last-event-seq id] version)))
     aggregate))
+
+(defn get-sequence-number-for-id
+  [ctx & [id]]
+  {:pre [(or (:id ctx)
+             id)]}
+  (dal/get-sequence-number-for-id
+    (if id
+      (assoc ctx :id id)
+      ctx)))
+
+(defn get-id-for-sequence-number
+  [ctx & [sequence]]
+  {:pre [(or (:sequence ctx)
+             sequence)]}
+  (dal/get-id-for-sequence-number
+    (if sequence
+      (assoc ctx :id sequence)
+      ctx)))
+
+(defn get-aggregate-id-by-identity
+  [ctx & [identity]]
+  {:pre [(or (:identity ctx)
+             identity)]}
+  (dal/get-aggregate-id-by-identity
+    (if identity
+      (assoc ctx :identity identity)
+      ctx)))
+
+
+(defn advanced-search
+  [ctx & [query]]
+  {:pre [(or (:query ctx)
+             query)]}
+  (search/advanced-search
+    (if query
+      (assoc ctx :query query)
+      ctx)))
+
+(defn simple-search
+  [ctx & [query]]
+  {:pre [(or (:query ctx)
+             query)]}
+  (search/simple-search
+    (if query
+      (assoc ctx :query query)
+      ctx)))
+
 
 
 (defn create-identity
