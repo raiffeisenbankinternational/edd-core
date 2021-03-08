af794b3966a5d9e631657e73b47e5b4911b8aa3a
Guenter Kickinger <wzhguki@ewp.rbi.cloud>
2021-03-08 18:25:05 +0100

    [LMSO-1650] Apply intermediate events when batching
    Change-Id: Ic2ff0c8d957c3a7cf0e648932f77f08fd8bccae7

--- test/edd/el/test_cmd_test.clj
+++ test/edd/el/test_cmd_test.clj
@@ -14,9 +14,9 @@
                 dal/log-dps (fn [ctx] ctx)
                 dal/get-max-event-seq (fn [ctx id] 0)]
     (let [ctx (cmd/resolve-dependencies-to-context
-                {:commands [{:cmd-id :test-cmd
-                             :id     cmd-id}]})]
-      (is (= {:dps-resolved [{}]}
+               {:commands [{:cmd-id :test-cmd
+                            :id     cmd-id}]})]
+      (is (= {:dps-resolved {}}
              (dissoc ctx :commands))))))
 
 (deftest test-prepare-context-for-command-local
@@ -30,16 +30,16 @@
                 dal/log-dps (fn [ctx] ctx)
                 dal/get-max-event-seq (fn [ctx id] 0)]
     (let [ctx (cmd/resolve-dependencies-to-context
-                {:dps      {:test-cmd
-                            {:test-value   (fn [cmd]
-                                             {:query-id :q1
-                                              :query    {}})
-                             :test-value-2 (fn [cmd]
-                                             {:query-id :q2
-                                              :query    {}})}}
-                 :commands [{:cmd-id :test-cmd
-                             :id     cmd-id}]})]
-      (is (= {:dps-resolved [{:test-value {:response true}}]}
+               {:dps      {:test-cmd
+                           {:test-value   (fn [cmd]
+                                            {:query-id :q1
+                                             :query    {}})
+                            :test-value-2 (fn [cmd]
+                                            {:query-id :q2
+                                             :query    {}})}}
+                :commands [{:cmd-id :test-cmd
+                            :id     cmd-id}]})]
+      (is (= {:dps-resolved {:test-value {:response true}}}
              (dissoc ctx
                      :dps
                      :commands))))))
@@ -50,23 +50,21 @@
 (deftest test-resolve-id
   "Test if id is properly resolved"
   (let [id (cmd/resolve-command-id
-             {}
-             {:cmd-id :dummy-cmd
-              :id     id-1}
-             0)]
+            {}
+            {:cmd-id :dummy-cmd
+             :id     id-1})]
     (is (= id {:cmd-id :dummy-cmd
                :id     id-1}))))
 
 (deftest test-resolve-id-when-id-override-present
   "Test if id is properly resolved when override present"
   (let [id (cmd/resolve-command-id
-             {:id-fn        {:dummy-cmd (fn [ctx cmd] (+ (:dps-1 ctx)
-                                                         (:dps-2 ctx)))}
-              :dps-1        2
-              :dps-resolved [{:dps-2 3}]}
-             {:cmd-id :dummy-cmd
-              :id     id-1}
-             0)]
+            {:id-fn        {:dummy-cmd (fn [ctx cmd] (+ (:dps-1 ctx)
+                                                        (:dps-2 ctx)))}
+             :dps-1        2
+             :dps-resolved {:dps-2 3}}
+            {:cmd-id :dummy-cmd
+             :id     id-1})]
     (is (= id {:cmd-id      :dummy-cmd
                :id          5
                :original-id id-1}))))
@@ -74,11 +72,8 @@
 (deftest test-resolve-id-when-id-override-returns-nil
   "Test if id is properly resolved when override method return nil, we should fallback to (:id cmd)"
   (let [id (cmd/resolve-command-id
-             {:id-fn {:dummy-cmd (fn [ctx cmd] nil)}}
-             {:cmd-id :dummy-cmd
-              :id     id-1}
-             0)]
+            {:id-fn {:dummy-cmd (fn [ctx cmd] nil)}}
+            {:cmd-id :dummy-cmd
+             :id     id-1})]
     (is (= id {:cmd-id :dummy-cmd
                :id     id-1}))))
-
-
