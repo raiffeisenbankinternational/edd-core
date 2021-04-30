7c0c702c5a45efa27de66c270d1707146b1ec75d
Robert POFUK <robert.pofuk@rbinternational.com>
2021-04-30 21:19:29 +0200

    [LMSO-2295] Recover sequence when fail
    Change-Id: Iabcac78f029034a7f94c52dec05a44809c86c084

--- test/edd/postgres/event_store_it.clj
+++ test/edd/postgres/event_store_it.clj
@@ -104,3 +104,55 @@
                 :interaction-id interaction-id
                 :request-id     request-id}
                resp))))))
+
+(deftest test-sequence-when-saving-error
+  (binding [*dal-state* (atom {})]
+    (let [invocation-id (uuid/gen)
+          ctx (-> (get-ctx invocation-id)
+                  (edd/reg-cmd :cmd-i1 (fn [ctx cmd]
+                                         [{:sequence (:id cmd)}
+                                          {:event-id :i1-created}]))
+                  (edd/reg-cmd :cmd-i2 (fn [ctx cmd]
+                                         [{:event-id :i1-created}])
+                               :dps {:seq edd.common/get-sequence-number-for-id})
+                  (edd/reg-fx (fn [ctx events]
+                                {:commands [{:cmd-id :cmd-i2
+                                             :id     (:id (first events))}]
+                                 :service  :s2})))
+          agg-id (uuid/gen)
+          interaction-id (uuid/gen)
+          request-id (uuid/gen)
+          agg-id-2 (uuid/gen)
+          req {:request-id     request-id
+               :interaction-id interaction-id
+               :commands       [{:cmd-id :cmd-i1
+                                 :id     agg-id}]}
+          resp (with-redefs [event-store/update-sequence (fn [ctx cmd]
+                                                           (throw (ex-info "Sequence Store failure" {})))]
+                 (edd/handler ctx
+                              (assoc req :log-level :debug)))]
+
+      (edd/handler ctx
+                   {:request-id     (uuid/gen)
+                    :interaction-id interaction-id
+                    :commands       [{:cmd-id :cmd-i1
+                                      :id     agg-id-2}]})
+      (edd/with-stores
+        ctx (fn [ctx]
+              (is (not= []
+                        (event-store/get-response-log ctx invocation-id)))
+              (let [seq (event-store/get-sequence-number-for-id-imp
+                         (assoc ctx :id agg-id))]
+                (is (> seq
+                       0))
+                (is (= (dec seq)
+                       (event-store/get-sequence-number-for-id-imp
+                        (assoc ctx :id agg-id-2)))))
+              (is (= nil
+                     (event-store/get-sequence-number-for-id-imp
+                      (assoc ctx :id (uuid/gen)))))))
+
+      (is (= {:error          "Sequence Store failure"
+              :interaction-id interaction-id
+              :request-id     request-id}
+             resp)))))
