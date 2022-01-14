53398516c9d6f61b64f12f7fa061c0ce9e34bc72
Robert POFUK <robert.pofuk@alpha-prosoft.com>
2022-01-14 01:12:21 +0100

    [LSO-534] Log memory diff
    Change-Id: Icc299b91378f32b84418fbd821a96c5c4242ae27

--- src/edd/response/s3.clj
+++ src/edd/response/s3.clj
@@ -8,27 +8,28 @@
 (defmethod cache-response
   :s3
   [{:keys [aws service-name] :as ctx} resp]
-  (log/info "Storing cache response")
-  (let [s3 {:s3 {:bucket {:name (str (:account-id aws)
-                                     "-"
-                                     (:environment-name-lower ctx)
-                                     "-sqs")}
-                 :object {:key (str "response/"
-                                    (:request-id ctx)
-                                    "/"
-                                    (str/join "-" (:breadcrumbs ctx))
-                                    "/"
-                                    (name service-name)
-                                    ".json")}}}
-        {:keys [error]} (s3/put-object
-                         ctx
-                         (assoc-in s3
-                                   [:s3 :object :content]
-                                   (util/to-json {:resp    resp
-                                                  :service service-name})))]
-    (if error
-      {:error error}
-      s3)))
+  (util/d-time
+   "Storing cache response"
+   (let [s3 {:s3 {:bucket {:name (str (:account-id aws)
+                                      "-"
+                                      (:environment-name-lower ctx)
+                                      "-sqs")}
+                  :object {:key (str "response/"
+                                     (:request-id ctx)
+                                     "/"
+                                     (str/join "-" (:breadcrumbs ctx))
+                                     "/"
+                                     (name service-name)
+                                     ".json")}}}
+         {:keys [error]} (s3/put-object
+                          ctx
+                          (assoc-in s3
+                                    [:s3 :object :content]
+                                    (util/to-json {:resp    resp
+                                                   :service service-name})))]
+     (if error
+       {:error error}
+       s3))))
 
 (defn register
   [ctx]
