15d1fe4a502948dfe8a70fac75ac29f04d3c59b2
Kamil Leszczuk <kamil.leszczuk-external@rbinternational.com>
2022-12-07 13:15:25 +0100

    [LSO-3213] Validate response schema
    Change-Id: I4518cd708e924c003cf3890ead83a85acdacbcc8

--- src/edd/el/query.clj
+++ src/edd/el/query.clj
@@ -1,15 +1,24 @@
 (ns edd.el.query
   (:require [clojure.tools.logging :as log]
+            [edd.schema :as schema]
             [malli.core :as m]
-            [malli.error :as me]
             [lambda.util :as util]))
+
+(defn- maybe-validate-response [ctx produces response]
+  (when (and (:validate-response-schema? ctx)
+             ;; Do not validate error responses.
+             (some? (:result response))
+             (not (m/validate produces response)))
+    (throw (ex-info "Invalid response"
+                    {:error (schema/explain-error produces response)}))))
 
 (defn handle-query
   [ctx body]
   (let [query (:query body)
         query-id (keyword (:query-id query))
         {:keys [consumes
-                handler]} (get-in ctx [:edd-core :queries query-id])]
+                handler
+                produces]} (get-in ctx [:edd-core :queries query-id])]
 
     (log/debug "Handling query" query-id)
     (when-not handler
@@ -18,10 +27,10 @@
                        :query-id query-id})))
     (when-not (m/validate consumes query)
       (throw (ex-info "Invalid request"
-                      {:error (-> (m/explain consumes query)
-                                  (me/humanize))})))
+                      {:error (schema/explain-error consumes query)})))
     (let [resp (util/d-time
                 (str "handling-query: " query-id)
                 (handler ctx query))]
       (log/debug "Query response" resp)
+      (maybe-validate-response ctx produces resp)
       resp)))
