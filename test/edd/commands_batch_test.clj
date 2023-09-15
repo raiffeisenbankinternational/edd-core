5ec4aec51573c183a61a4f2b6d418ee2325ee704
Robert POFUK <robert.pofuk@rbinternational.com>
2023-09-15 13:02:02 +0200

    [LSO-6047] Fix get-by-id cache
    Get by id should be callend only once. When we retry
    command we empty cache but should fil it in with next
    fetch by id
    
    Change-Id: I7c2dbb3c4d75c8ca5412df21c37b9126d615efea

--- test/edd/commands_batch_test.clj
+++ test/edd/commands_batch_test.clj
@@ -2,17 +2,15 @@
   (:require [clojure.test :refer :all]
             [lambda.util :as util]
             [lambda.uuid :as uuid]
-            [lambda.test.fixture.client :refer [verify-traffic
-                                                verify-traffic-edn]
-             :as client-mock]
             [lambda.test.fixture.core :refer [mock-core]]
             [edd.core :as edd]
             [edd.el.cmd :as edd-cmd]
+            [edd.common :as edd-common]
+            [edd.el.ctx :as el-ctx]
+            [edd.el.event :as edd-events]
             [lambda.core :as core]
             [edd.test.fixture.dal :as mock]
-            [edd.memory.event-store :as event-store]
-            [edd.memory.view-store :as view-store]
-            [lambda.test.fixture.client :as client]
+            [lambda.test.fixture.client :as mock-client]
             [sdk.aws.common :as common]
             [sdk.aws.sqs :as sqs]))
 
@@ -103,34 +101,34 @@
        (core/start
         ctx
         edd/handler)
-       (verify-traffic-edn [{:body   [{:result         {:success    true,
-                                                        :effects    [],
-                                                        :events     1,
-                                                        :meta       [{:cmd-1 {:id agg-id}}],
-                                                        :identities 0,
-                                                        :sequences  0}
-                                       :invocation-id  0
-                                       :request-id     req-id1
-                                       :interaction-id int-id}
-                                      {:error          [{:error "failed",
-                                                         :id    agg-id}],
-                                       :invocation-id  0
-                                       :request-id     req-id2
-                                       :interaction-id int-id}
-                                      {:result         {:success    true,
-                                                        :effects    [],
-                                                        :events     1,
-                                                        :meta       [{:cmd-1 {:id agg-id}}],
-                                                        :identities 0,
-                                                        :sequences  0}
-                                       :invocation-id  0
-                                       :request-id     req-id3
-                                       :interaction-id int-id}]
-                             :method :post
-                             :url    "http://mock/2018-06-01/runtime/invocation/0/response"}
-                            {:method  :get
-                             :timeout 90000000
-                             :url     "http://mock/2018-06-01/runtime/invocation/next"}]))
+       (mock-client/verify-traffic-edn [{:body   [{:result         {:success    true,
+                                                                    :effects    [],
+                                                                    :events     1,
+                                                                    :meta       [{:cmd-1 {:id agg-id}}],
+                                                                    :identities 0,
+                                                                    :sequences  0}
+                                                   :invocation-id  0
+                                                   :request-id     req-id1
+                                                   :interaction-id int-id}
+                                                  {:error          [{:error "failed",
+                                                                     :id    agg-id}],
+                                                   :invocation-id  0
+                                                   :request-id     req-id2
+                                                   :interaction-id int-id}
+                                                  {:result         {:success    true,
+                                                                    :effects    [],
+                                                                    :events     1,
+                                                                    :meta       [{:cmd-1 {:id agg-id}}],
+                                                                    :identities 0,
+                                                                    :sequences  0}
+                                                   :invocation-id  0
+                                                   :request-id     req-id3
+                                                   :interaction-id int-id}]
+                                         :method :post
+                                         :url    "http://mock/2018-06-01/runtime/invocation/0/response"}
+                                        {:method  :get
+                                         :timeout 90000000
+                                         :url     "http://mock/2018-06-01/runtime/invocation/next"}]))
       (is (= [{:Records [{:key (str "response/"
                                     req-id3
                                     "/0/local-test.json")}]}
@@ -164,29 +162,29 @@
        (core/start
         ctx
         edd/handler)
-       (verify-traffic-edn [{:body   [{:result         {:success    true,
-                                                        :effects    [],
-                                                        :events     1,
-                                                        :meta       [{:cmd-1 {:id agg-id}}],
-                                                        :identities 0,
-                                                        :sequences  0}
-                                       :invocation-id  0
-                                       :request-id     req-id4,
-                                       :interaction-id int-id}
-                                      {:result         {:success    true,
-                                                        :effects    [],
-                                                        :events     1,
-                                                        :meta       [{:cmd-1 {:id agg-id}}],
-                                                        :identities 0,
-                                                        :sequences  0}
-                                       :invocation-id  0
-                                       :request-id     req-id5,
-                                       :interaction-id int-id}]
-                             :method :post
-                             :url    "http://mock/2018-06-01/runtime/invocation/0/response"}
-                            {:method  :get
-                             :timeout 90000000
-                             :url     "http://mock/2018-06-01/runtime/invocation/next"}])))
+       (mock-client/verify-traffic-edn [{:body   [{:result         {:success    true,
+                                                                    :effects    [],
+                                                                    :events     1,
+                                                                    :meta       [{:cmd-1 {:id agg-id}}],
+                                                                    :identities 0,
+                                                                    :sequences  0}
+                                                   :invocation-id  0
+                                                   :request-id     req-id4,
+                                                   :interaction-id int-id}
+                                                  {:result         {:success    true,
+                                                                    :effects    [],
+                                                                    :events     1,
+                                                                    :meta       [{:cmd-1 {:id agg-id}}],
+                                                                    :identities 0,
+                                                                    :sequences  0}
+                                                   :invocation-id  0
+                                                   :request-id     req-id5,
+                                                   :interaction-id int-id}]
+                                         :method :post
+                                         :url    "http://mock/2018-06-01/runtime/invocation/0/response"}
+                                        {:method  :get
+                                         :timeout 90000000
+                                         :url     "http://mock/2018-06-01/runtime/invocation/next"}])))
     (is (= [{:Records [{:key (str "response/"
                                   req-id5
                                   "/0/local-test.json")}]}
@@ -266,7 +264,7 @@
                 :url     "http://mock/2018-06-01/runtime/invocation/next"}]
               (map
                #(dissoc % :headers :keepalive)
-               (client-mock/traffic-edn)))))
+               (mock-client/traffic-edn)))))
       (is (= [{:Records [{:key (str "response/"
                                     req-id3
                                     "/0/local-test.json")}]}
@@ -345,7 +343,7 @@
                  :url     "http://mock/2018-06-01/runtime/invocation/next"}]
                (map
                 #(dissoc % :headers :keepalive)
-                (client-mock/traffic-edn)))))
+                (mock-client/traffic-edn)))))
       (is (= [{:Records [{:key (str "response/"
                                     req-id3
                                     "/0/local-test.json")}]}
@@ -408,4 +406,26 @@
                  :url     "http://mock/2018-06-01/runtime/invocation/next"}]
                (map
                 #(dissoc % :headers :keepalive)
-                (client-mock/traffic-edn))))))))
+                (mock-client/traffic-edn))))))))
+
+(deftest test-concurent-update
+  (let [versions (atom 72)
+        ctx (-> mock/ctx
+                (edd/reg-cmd :cmd-1 (fn [ctx _cmd]
+                                      (is (= (:version (el-ctx/get-aggregate ctx))
+                                             (:version (:agg ctx)))))
+                             :deps {:agg (fn [_ctx cmd]
+                                           {:query-id :get-agg-by-id
+                                            :id (:id cmd)})})
+                (edd/reg-query :get-agg-by-id edd-common/get-by-id))]
+    (with-redefs [common/create-date (fn [] "20210322T232540Z")
+
+                  sqs/sqs-publish (fn [{:keys [_message]}]
+                                    (throw (RuntimeException. "Publihsing something")))
+                  edd-events/fetch-snapshot (fn [_ctx id]
+                                              (swap! versions inc)
+                                              {:id id
+                                               :version @versions})]
+      (mock/with-mock-dal
+        (mock/handle-cmd ctx {:cmd-id :cmd-1
+                              :id (uuid/gen)})))))
