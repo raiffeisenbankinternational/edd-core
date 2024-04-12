b485f3ca4ef5e9f2429db05be614340a9460a84d
Karol WOJCIK <karol.wojcik-external@rbinternational.com>
2024-04-12 11:16:16 +0200

    [LSO-9554] Allow user to publish custom metrics in EMF event
    Change-Id: Ibf23aefc4d5eb26b01c23307e7dbd47f185b8eea

--- test/lambda/emf_test.clj
+++ test/lambda/emf_test.clj
@@ -7,4 +7,35 @@
 
 (deftest emf-test
   (testing "AWS Embedded Metric Event should conform to the specification"
-    (is (= nil (me/humanize (m/explain emf/AWSEvent (emf/->event)))))))
+    (is (= nil (me/humanize (m/explain emf/AWSEvent (emf/->event))))))
+  (testing "Publishing of custom event"
+    (let [event (emf/->event [{"Name" "FacilitiesCount"
+                               "Value" 30000
+                               "Unit" "Count"
+                               "StorageResolution" 60}])]
+      (is (= 30000 (get event "FacilitiesCount")))
+      (is (= [{"Name" "MemoryInUse",
+               "Unit" "Megabytes",
+               "StorageResolution" 60}
+              {"Name" "MemoryAvailable",
+               "Unit" "Megabytes",
+               "StorageResolution" 60}
+              {"Name" "MemoryTotal",
+               "Unit" "Megabytes",
+               "StorageResolution" 60}
+              {"Name" "GCTotalTime",
+               "Unit" "Milliseconds",
+               "StorageResolution" 60}
+              {"Name" "GCTotalCount",
+               "Unit" "Count",
+               "StorageResolution" 60}
+              {"Name" "GCTotalTimeAggregated",
+               "Unit" "Milliseconds",
+               "StorageResolution" 60}
+              {"Name" "GCTotalCountAggregated",
+               "Unit" "Count",
+               "StorageResolution" 60}
+              {"Name" "FacilitiesCount",
+               "Unit" "Count",
+               "StorageResolution" 60}]
+             (get-in event ["_aws" "CloudWatchMetrics" 0 "Metrics"]))))))
