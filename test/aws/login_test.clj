472ee72b3f715ced97e8652eb4e879e9aca7879a
Ivan GRISHAEV <ivan.grishaev-external@rbinternational.com>
2024-07-01 16:17:32 +0300

    [LSP-7899] Move login function into edd core
    Change-Id: Ibc50f48fee437bb7ef286b295a1273f5506189eb

---
+++ test/aws/login_test.clj
@@ -0,0 +1,51 @@
+(ns aws.login-test
+  (:require
+   [aws.login :as login]
+   [clojure.test :refer [deftest is]]
+   [lambda.util :as util]))
+
+(deftest test-get-id-token-ok
+
+  (let [capture!
+        (atom nil)
+
+        result
+        (with-redefs [util/http-get
+                      (fn [url params opt]
+                        (reset! capture! {:url url
+                                          :params params
+                                          :opt opt})
+                        {:status 200
+                         :body (util/to-json {:id-token "secret-token"})})]
+
+          (login/get-id-token "john"
+                              "Smith123"
+                              "lime-dev19.internal.rbigroup.cloud"))]
+
+    (is (= "secret-token" result))
+
+    (is (= {:url "https://glms-login-svc.lime-dev19.internal.rbigroup.cloud"
+            :params
+            {:query-params {"json" "true" "user-password-auth" "true"}
+             :body "{\"username\":\"john\",\"password\":\"Smith123\"}"}
+            :opt {:raw true}}
+           @capture!))))
+
+(deftest test-get-id-token-error
+
+  (try
+    (with-redefs [util/http-get
+                  (constantly {:status 301 :body ""})]
+
+      (login/get-id-token "john"
+                          "Smith123"
+                          "lime-dev19.internal.rbigroup.cloud"))
+    (is false)
+    (catch Throwable e
+      (is (= "GLMS login service error, status: 301, url: https://glms-login-svc.lime-dev19.internal.rbigroup.cloud, user: john"
+             (ex-message e)))
+      (is (= {:username "john"
+              :url "https://glms-login-svc.lime-dev19.internal.rbigroup.cloud"
+              :status 301
+              :body ""}
+             (ex-data e))))))
