c58c202a5ae20eacd760c70ee7a5c2ac2d94a268
Ivan Grishaev <ivan@grishaev.me>
2024-07-04 14:50:11 +0300

    [LSP-7934] Add Excel capabilities to edd-core
    Change-Id: I33e00b0fbcc27a32b92485e5d4837aca79eb3906

---
+++ modules/edd-core-xlsx/src/edd/xlsx/core.clj
@@ -0,0 +1,163 @@
+(ns edd.xlsx.core
+  "
+  A thin layer on top of great Fastexcel and Fastexcel-reader libraries
+  for writing and reading Excel files. Both compile by GraalVM with no issues.
+  As Docjure depedepnds on POI, it is impossible to compile it.
+
+  See:
+  - https://github.com/dhatim/fastexcel
+  - https://javadoc.io/doc/org.dhatim/fastexcel/latest/
+  - https://javadoc.io/doc/org.dhatim/fastexcel-reader/
+
+  "
+  (:import
+   (clojure.lang Keyword)
+   (java.io InputStream
+            OutputStream)
+   (java.time LocalDate)
+   (org.dhatim.fastexcel StyleSetter
+                         Workbook
+                         Worksheet)
+   (org.dhatim.fastexcel.reader ReadableWorkbook
+                                Sheet
+                                Row))
+  (:require
+   [clojure.java.io :as io]))
+
+(set! *warn-on-reflection* true)
+
+;; Setting the right mime type is important because AWS and Outlook
+;; can rename the file to *.xls (with no 'x' at the end) after it has
+;; been downloaded. It will prevent Excel from opening such a file.
+(def MIME-TYPE
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
+(defn enumerate [coll]
+  (map-indexed vector coll))
+
+(defmulti set-value
+  "
+  A custom method to write a value into a cell. Mostly used
+  to prevent reflection warnings and bypass some corner cases
+  (see comments below).
+  "
+  (fn [_ws _i _j x]
+    (type x)))
+
+(defmethod set-value nil
+  ;; just do nothing to prevent NPE
+  [_ws _i _j _x])
+
+(defmethod set-value Keyword
+  ;; keywords are not supported by default, add them
+  [^Worksheet ws ^Integer i ^Integer j ^Keyword x]
+  (.value ws i j (str x)))
+
+(defmethod set-value String
+  [^Worksheet ws ^Integer i ^Integer j ^String x]
+  (.value ws i j x))
+
+(defmethod set-value Number
+  [^Worksheet ws ^Integer i ^Integer j ^Number x]
+  (.value ws i j x))
+
+(defmethod set-value Boolean
+  [^Worksheet ws ^Integer i ^Integer j ^Boolean x]
+  (.value ws i j x))
+
+(defmethod set-value LocalDate
+  ;; coerce to string as it is stored as an integer
+  [^Worksheet ws ^Integer i ^Integer j ^LocalDate x]
+  (.value ws i j (str x)))
+
+(defn write-matrix
+  "
+  Dump a plain matrix (a vector of vectors) into an output stream.
+  The first line is considered a header and has a custom style.
+  "
+  ([matrix sheet-title output-stream]
+   (write-matrix matrix sheet-title output-stream nil))
+
+  ([matrix
+    ^String sheet-title
+    ^OutputStream output-stream
+    {:keys [zoom
+            version
+            application
+            header-color]
+     :or {zoom 150
+          version "1.0"
+          application "clojure"
+          header-color "eeeeee"}}]
+
+   (with-open [wb (new Workbook
+                       output-stream
+                       application
+                       version)]
+
+     (let [ws (.newWorksheet wb sheet-title)]
+
+       (.setZoom ws zoom)
+
+       (doseq [[i row] (enumerate matrix)
+               [j cell] (enumerate row)]
+
+         (set-value ws i j cell)
+
+         (when (zero? i)
+           (-> ws
+               ^StyleSetter (.style 0 j)
+               ^StyleSetter (.bold)
+               ^StyleSetter (.fillColor header-color)
+               (.set))))))))
+
+(defn write-matrix-to-file
+  "
+  Like write-matrix but write into a file by its path.
+  "
+  ([matrix sheet-title file-path]
+   (write-matrix-to-file matrix sheet-title file-path nil))
+
+  ([matrix sheet-title file-path options]
+   (with-open [output-stream (-> file-path
+                                 io/file
+                                 io/output-stream)]
+     (write-matrix matrix
+                   sheet-title
+                   output-stream
+                   options))))
+
+(defn read-matrix
+  "
+  Having an input stream with Excel payload, read the given sheet
+  (first when not set) into a matrix, a vector of vectors or strings.
+  "
+  ([input-stream]
+   (read-matrix input-stream nil))
+
+  ([^InputStream input-stream
+    {:keys [sheet-index]
+     :or {sheet-index 0}}]
+
+   (with-open [wb (new ReadableWorkbook input-stream)]
+     (let [^Sheet sheet
+           (-> wb (.getSheet sheet-index) (.get))]
+       (vec
+        (for [^Row row (.read sheet)]
+          (let [cell-count (.getCellCount row)]
+            (vec
+             (for [j (range 0 cell-count)]
+               (.orElse (.getCellRawValue row j) nil))))))))))
+
+(defn read-matrix-from-file
+  "
+  Like read-matrix but read from a file by its path.
+  "
+  ([file-path]
+   (read-matrix-from-file file-path nil))
+
+  ([file-path options]
+   (with-open [in (-> file-path
+                      io/file
+                      io/input-stream)]
+     (read-matrix in options))))
